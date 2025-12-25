1da74200d82b7b55862a48e417415487317ea7e8
arbajpatel0977 <111721964+arbajpatel0977@users.noreply.github.com>
2025-12-25 18:53:28 +0530

    Change background colors in BottomBanner component
    Updated icon and text background colors to 'bg-primary' for consistency.

--- albaithGroceryApp/client/src/components/BottomBanner.jsx
+++ albaithGroceryApp/client/src/components/BottomBanner.jsx
@@ -12,9 +12,9 @@ const BottomBanner = () => {
                     <h1 className='text-2xl md:text-3xl font-semibold text-primary mb-6'>Why We Are The Best </h1>
                     {features.map((feature, index) => (
                         <div key={index} >
-                            <img src={feature.icon} alt={feature.title} className='md:w-11 w-9 bg-green-700' />
-                            <div> <h3 className='text-lg md:text-xl font-semibold text-green-700'>{feature.title}</h3>
-                                <p className=' text-xs md:text-sm text-green-700'>{feature.description}</p>
+                            <img src={feature.icon} alt={feature.title} className='md:w-11 w-9 bg-primary' />
+                            <div> <h3 className='text-lg md:text-xl font-semibold bg-primary'>{feature.title}</h3>
+                                <p className=' text-xs md:text-sm bg-primary'>{feature.description}</p>
                             </div>
                         </div>
                     ))}
@@ -23,5 +23,6 @@ const BottomBanner = () => {
         </div>
     )
 }
+
 
 export default BottomBanner
