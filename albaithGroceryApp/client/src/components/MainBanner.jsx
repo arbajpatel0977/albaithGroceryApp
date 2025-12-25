bbd6c6f54479b89e28c1c90a861cee37eb6fc699
arbajpatel0977 <111721964+arbajpatel0977@users.noreply.github.com>
2025-12-25 18:51:12 +0530

    Change text color in MainBanner component

--- albaithGroceryApp/client/src/components/MainBanner.jsx
+++ albaithGroceryApp/client/src/components/MainBanner.jsx
@@ -8,18 +8,19 @@ const MainBanner = () => {
             <img src={assets.banner_big} alt="" className='w-full hidden md:block' />
             <img src={assets.banner_small} alt="" className='w-full md:hidden' />
             <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24 '>
-                <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15 text-green-600'>Freshness You Can Trust, Savings You will Love!</h1>
+                <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15 text-primary'>Freshness You Can Trust, Savings You will Love!</h1>
 
                 <div className='flex items-center mt-6 font-medium'>
                     <Link to={'/products'} className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer">Shop Now
                     </Link>
 
-                    <Link to={'/products'} className="group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer text-green-600">Explore deals
+                    <Link to={'/products'} className="group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer text-primary">Explore deals
                     </Link>
                 </div>
             </div>
         </div>
     )
 }
+
 
 export default MainBanner
